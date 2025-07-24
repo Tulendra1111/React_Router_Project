540ffb50941351578e006c6e74fc5681df9e26e7
Tulendra Chandrakar <tulendrachandrakar1111@gmail.com>
2025-07-24 18:19:34 +0530

    initial commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfLeapXgeOyo4gg1T4MAiQ9i2DH4Ea2mGVqg&s'} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Tulendra1111')
+    return response.json()
+}
